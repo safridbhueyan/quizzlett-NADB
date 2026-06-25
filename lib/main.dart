f20ff94db987e6b885baccb259e162675668daac
Safrid Bhueyan <safridbhueyan@gmail.com>
2026-06-26 00:24:32 +0600

    initialize Firebase, implement `themeModeProvider` in `main.dart`, and update `AppTheme` configuration

--- lib/main.dart
+++ lib/main.dart
@@ -5,28 +5,28 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:quizlett/core/constant/default_screensize.dart';
 import 'package:quizlett/core/routes/route_config.dart';
-import 'package:quizlett/core/theme/app_theme.dart';
+import 'package:quizlett/core/constant/app_theme.dart';
+import 'package:quizlett/core/constant/theme_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-/// GoRouter provider so it can access Riverpod state for auth-guarded redirects.
 final _routerProvider = Provider<GoRouter>((ref) {
   return RouteConfig(ref).goRouter;
 });
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
-  ///set device orientation to portraitUp during app running for better user experience of the UI
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-  ///ensuring screen size for screen util package to implement pixel perfect UI
   await ScreenUtil.ensureScreenSize();
 
   runApp(const ProviderScope(child: MyApp()));
 
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent, // Transparent status bar
-      systemNavigationBarColor: Colors.transparent, // Transparent nav bar
+      statusBarColor: Colors.transparent,
+      systemNavigationBarColor: Colors.transparent,
       systemNavigationBarIconBrightness: Brightness.light,
       statusBarIconBrightness: Brightness.light,
     ),
@@ -39,6 +39,7 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(_routerProvider);
+    final themeMode = ref.watch(themeModeProvider);
 
     return ScreenUtilInit(
       designSize: const Size(
@@ -52,9 +53,9 @@ class MyApp extends ConsumerWidget {
           routerConfig: router,
           title: 'Quizlett',
           debugShowCheckedModeBanner: false,
-          darkTheme: AppTheme.darkTheme,
-          themeMode: ThemeMode.dark,
-          theme: AppTheme.darkTheme,
+          theme: AppTheme.light,
+          darkTheme: AppTheme.dark,
+          themeMode: themeMode,
         );
       },
     );
