ebdccdc97f66f4998df38146572a4ae0d28d5523
Safrid Bhueyan <safridbhueyan@gmail.com>
2026-06-26 00:22:11 +0600

    add Google services Gradle plugin to root-level build file

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,7 +4,13 @@ allprojects {
         mavenCentral()
     }
 }
+plugins {
+  // ...
 
+  // Add the dependency for the Google services Gradle plugin
+  id("com.google.gms.google-services") version "4.5.0" apply false
+
+}
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
